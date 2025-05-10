e3017c21ca2c38aa4ecd2f1f87858fcb0533404a
Kunal Bhalerao <kunalbhalerao2004@gmail.com>
2025-05-10 22:49:18 +0530

    Add file
    This is the java code for finding the greatest number in between two , this code consist of simple condition which specify which number is grater.

---
+++ GratestNumber.java
@@ -0,0 +1,25 @@
+import java.util.*;
+	 	
+	class GratestNumber
+{
+	/* This code is about to find the gratest number in between 2 number which are define the user */
+
+	public static void main (String[] args)
+  {
+	Scanner sc = new Scanner (System.in);
+	
+	System.out.println ("Enter your first number"); //Taking first Number input.
+	int FirstNumber = sc.nextInt();
+	System.out.println ("Enter your second number");// Taking Second Number input.
+	int SecondNumber = sc.nextInt();
+
+	if (FirstNumber > SecondNumber) // Condition to filter the number.
+    {
+	System.out.println ("The gratest number is " + FirstNumber );
+    }
+	else
+    {
+	System.out.println ("The gratest number is " + SecondNumber );
+    }
+  }
+}
