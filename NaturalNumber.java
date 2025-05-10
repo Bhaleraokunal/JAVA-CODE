67616caebd0366fc9c4d6d827ef83ab59fb3105f
Kunal Bhalerao <kunalbhalerao2004@gmail.com>
2025-05-10 22:06:35 +0530

    Add file
    Calculates the sum of 10 consecutive natural numbers starting from a user-input value using the arithmetic series formula for efficiency

---
+++ NaturalNumber.java
@@ -0,0 +1,14 @@
+import java.util.*;
+	class NaturalNumber
+{
+	public static void main(String[]args)
+{
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Starting number :");
+	int start= sc.nextInt();
+	int n = 10;
+	int end = start + n -1;
+	int sum = n*(start + end)/2;
+	System.out.println(sum);
+}
+}
