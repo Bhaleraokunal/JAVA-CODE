c06af31c1f2a74a066ebd1e3cf7c8a1a9bc99146
Kunal Bhalerao <kunalbhalerao2004@gmail.com>
2025-05-12 21:19:48 +0530

    Update FisrtNatural.java

--- FisrtNatural.java
+++ FisrtNatural.java
@@ -2,19 +2,19 @@ import java.util.*;
 	public class FisrtNatural
 {
 	public static void main ( String[] args)
-{
+   {
 	Scanner sc = new Scanner (System.in);
 	System.out.println ("Enter your Number : ");
 	int num = sc.nextInt();
 
 	if(num<=0)
-{
+      {
 	System.out.println ( " Please Enter the Possitive Number"+ num);
-}
+      }
 	else
-{
+      {
 	int sum = num*(num+1)/2;
 	System.out.println ("The Sum Of First Natural Number Is : " + sum);
-}
-}
+      }
+   }
 }
