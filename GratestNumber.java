9e6e6dd8e396df636bc98b817678945c1b93c7b3
Kunal Bhalerao <kunalbhalerao2004@gmail.com>
2025-05-12 21:20:29 +0530

    Update GratestNumber.java

--- GratestNumber.java
+++ GratestNumber.java
@@ -5,7 +5,7 @@ import java.util.*;
 	/* This code is about to find the gratest number in between 2 number which are define the user */
 
 	public static void main (String[] args)
-  {
+    {
 	Scanner sc = new Scanner (System.in);
 	
 	System.out.println ("Enter your first number"); //Taking first Number input.
@@ -14,12 +14,12 @@ import java.util.*;
 	int SecondNumber = sc.nextInt();
 
 	if (FirstNumber > SecondNumber) // Condition to filter the number.
-    {
+      {
 	System.out.println ("The gratest number is " + FirstNumber );
-    }
+      }
 	else
-    {
+      {
 	System.out.println ("The gratest number is " + SecondNumber );
-    }
-  }
+      }
+   }
 }
