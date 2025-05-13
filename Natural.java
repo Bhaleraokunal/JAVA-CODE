940ea9f09a21f22e85c0b198d53f82981af727bf
Kunal Bhalerao <kunalbhalerao2004@gmail.com>
2025-05-13 08:34:03 +0530

    Update Natural.java

--- Natural.java
+++ Natural.java
@@ -3,7 +3,7 @@ import java.util.*;
 	public class Natural
 {
 	public static void main (String[] args)
-{
+    {
 	Scanner sc = new Scanner (System.in);
 	
 	System.out.println ("Enter you number : ");
@@ -12,5 +12,5 @@ import java.util.*;
 	int sum = num*(num+1)/2;
 
 	System.out.println ( "The sum of first n natural Number is " + sum);
-}
+    }
 }
