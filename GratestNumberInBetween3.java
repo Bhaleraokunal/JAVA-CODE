02ff0c6ce81bc6b3f4729cc1f744291d15a17d37
Kunal Bhalerao <kunalbhalerao2004@gmail.com>
2025-05-13 08:33:30 +0530

    Update GratestNumberInBetween3.java

--- GratestNumberInBetween3.java
+++ GratestNumberInBetween3.java
@@ -2,7 +2,7 @@ import java.util.*;
 	class GratestNumberInBetween3
 {
 	public static void main (String [] args)
-{
+   {
 	Scanner sc = new Scanner(System.in);
 	System.out.println ("Enter your first number : ");
 	int number1 = sc.nextInt();
@@ -12,16 +12,16 @@ import java.util.*;
 	int number3 = sc.nextInt();
 
 	if (number1 >= number2 && number1 >= number3)
-{
+      {
 	System.out.println ("The gratest number is : " + number1);
-}
+      }
 	else if (number2 >= number1 && number2 >= number3)
-{
+       {
 	System.out.println ("The gratest number is:" + number2);
-}
+       }
 	else 
-{
+       {
 	System.out.println("The gratest number is :" + number3);
-}
-}
+       }
+   }
 }
