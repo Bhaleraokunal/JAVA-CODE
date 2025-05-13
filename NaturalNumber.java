2125464122546aac8c3ab03d0e65d66b458f24b7
Kunal Bhalerao <kunalbhalerao2004@gmail.com>
2025-05-13 08:34:28 +0530

    Update NaturalNumber.java

--- NaturalNumber.java
+++ NaturalNumber.java
@@ -2,7 +2,7 @@ import java.util.*;
 	class NaturalNumber
 {
 	public static void main(String[]args)
-{
+    {
 	Scanner sc = new Scanner(System.in);
 	System.out.println("Starting number :");
 	int start= sc.nextInt();
@@ -10,5 +10,5 @@ import java.util.*;
 	int end = start + n -1;
 	int sum = n*(start + end)/2;
 	System.out.println(sum);
-}
+    }
 }
