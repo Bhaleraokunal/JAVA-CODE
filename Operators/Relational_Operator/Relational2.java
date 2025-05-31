512c8de0a3af6afbe418e09fa4d91097bad13bb6
Kunal Bhalerao <kunalbhalerao2004@gmail.com>
2025-05-31 21:47:12 +0530

    Add file
    This is file contain the code for relational operator, describe how it can be used and which data types we can use for relational operator.

---
+++ Operators/Relational_Operator/Relational2.java
@@ -0,0 +1,20 @@
+class  Relational2
+{
+	public static void main(String[] args) 
+	{
+		System.out.println ( 10> 20); // false
+		System.out.println ( 10 < 20 ); // true
+		System.out.println ( 0 == 0.0 ); //true
+		System.out.println ( 159.999 == 60 ); //false
+		System.out.println ( 97!=100); // ture
+		System.out.println ( 'a' < 'A' ); // false
+		System.out.println ( 'z' <= 122 ); //true
+		// System.out.println ( false < true ); CTE
+		// System.out.println ( false > ture);  CTE
+		System.out.println ( true != true ); // false
+		System.out.println ( false != false ); // false
+		System.out.println (22/7 > 22.0f/7.0f ); // false
+		System.out.println ( 22/7 );
+		System.out.println ( 22.0/7.0 );
+	}
+}
