2e66efce3c88849cfd1a64642e78821724d3138b
Kunal Bhalerao <kunalbhalerao2004@gmail.com>
2025-05-31 21:27:30 +0530

    Add file

---
+++ Operators/Relational_Operator/Relational.java
@@ -0,0 +1,14 @@
+class Relational 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println ( 10 == 10 ); //ture
+		System.out.println ( 10 > 10 ); // false
+		System.out.println ( 10 < 10 ); // false
+		System.out.println ( 10 <= 10 ); // ture
+		System.out.println ( 10 >= 10 ); // ture
+		System.out.println ( 'a' > 'A' ); // ture
+		System.out.println ( 10 != 10 ); // false
+		
+	}
+}
