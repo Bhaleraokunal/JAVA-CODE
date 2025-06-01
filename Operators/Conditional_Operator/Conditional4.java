d2b00ff15a2b7c5d7a2fd9e9ea57a630c0602df4
Kunal Bhalerao <kunalbhalerao2004@gmail.com>
2025-06-01 17:15:48 +0530

    Add file
    How to find the number is positive or negative without using the loops. ?

---
+++ Operators/Conditional_Operator/Conditional4.java
@@ -0,0 +1,13 @@
+class Conditional4 
+{
+	public static void main(String[] args) 
+	{
+		// Find the number is negative or positive without using the loop.? 
+		
+		int number = 9;
+		
+		String ans = number<0 ? "Negative Number" : "Positive Number";
+		
+		System.out.println ("the given number is"  + ans) ;
+	}
+}
