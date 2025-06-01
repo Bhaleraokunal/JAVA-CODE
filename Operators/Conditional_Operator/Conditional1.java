6530090901f5ed7737d77c378c20d251ca7829a2
Kunal Bhalerao <kunalbhalerao2004@gmail.com>
2025-06-01 17:06:41 +0530

    Add file
    This code contain how efficiently you can use the "conditional operator".

---
+++ Operators/Conditional_Operator/Conditional1.java
@@ -0,0 +1,14 @@
+class Conditional1 
+{
+	public static void main(String[] args) 
+	{
+		//Finding the gratest of two number without using the loop.
+		
+		int n1 = 41;
+		int n2 = 40;
+		
+		int ans = n1>n2? n1 : n2;
+		
+		System.out.println("The answer is" + ans);
+	}
+}
---
+++ Operators/Conditional_Operator/Conditional2.java
@@ -0,0 +1,13 @@
+class Conditional2 
+{
+	public static void main(String[] args) 
+	{
+		// Find the even odd number without using the loops.?
+		
+		int num1 = 10000; // Taking Number for input 
+		
+		String ans = num1%2==0? "this is even" : "The given number is even"; // Use the conditional operator to get the expected output.
+		
+		System.out.println(ans); // Printing the output.
+	}
+}
