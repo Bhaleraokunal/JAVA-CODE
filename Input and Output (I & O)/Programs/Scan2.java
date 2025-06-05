98e286a9716feb8713c0dc6d426a23fe51d19f01
Kunal Bhalerao <kunalbhalerao2004@gmail.com>
2025-06-05 22:22:21 +0530

    Add file via upload
    This file contains code that demonstrates how to take user input in Java using the Scanner class.
    It is a simple program that collects user details (like name, age, etc.) from the user through console input.

---
+++ Input and Output (I & O)/Programs/Scan2.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+class Scan2 
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner (System.in);
+		
+		System.out.println("Wnter your first name :");
+		String firstName = sc.nextLine();
+		
+		System.out.println("Enter your last name :");
+		String lastName = sc.nextLine();
+		
+		System.out.println("your full name is"+ firstName + " " + lastName);
+		
+		System.out.println("Enter your age:");
+		int age = sc.nextInt();
+		
+		sc.nextLine();
+		
+		System.out.println("Enter your location : ");
+		String location = sc.nextLine();
+		
+	}
+}
