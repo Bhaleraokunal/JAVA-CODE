bc35e7dc91ba7a6720af6773091f53c1ab72c302
Kunal Bhalerao <kunalbhalerao2004@gmail.com>
2025-06-10 20:57:25 +0530

    Add file via upload

---
+++ Codes/Sample.java
@@ -0,0 +1,17 @@
+class Sample 
+{
+	public static void main(String[] args) 
+	{
+		int start = 1;
+		int sum = 0 ; 
+		int end = 4;
+		
+		while (start<=end)
+		{
+			sum = sum + start;
+			start++;
+			
+		}
+			System.out.println("The value of sum is :"+ sum);
+	}
+}
