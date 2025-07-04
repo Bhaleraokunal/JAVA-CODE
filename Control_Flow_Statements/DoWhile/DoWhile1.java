e870c79d6a6364eb6931dfc6fb0efda572657503
Kunal Bhalerao <kunalbhalerao2004@gmail.com>
2025-07-04 19:33:39 +0530

    Add file via upload

---
+++ Control_Flow_Statements/DoWhile/DoWhile1.java
@@ -0,0 +1,14 @@
+class DoWhile1 
+{
+	public static void main(String[] args) 
+	{
+		int start = 0;
+		do
+		{
+			System.out.println("Good Mornig!");
+			start++;
+		}
+		while (start < 5);
+		System.out.println("Hello World!");
+	}
+}
