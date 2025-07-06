31100924c24b5ecdcd874c4ce5677a6032bc365a
Kunal Bhalerao <kunalbhalerao2004@gmail.com>
2025-07-06 19:02:25 +0530

    Add file via upload

---
+++ Codes/Numbers Programs/MissingDigitCode/FrequancyOfNumber.java
@@ -0,0 +1,38 @@
+import java.util.Scanner ;
+class FrequancyOfNumber 
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		int max=-1;
+		int min=10;
+		boolean flag = false;
+		for (int i=n;i>0 ;i/=10 )
+		{
+			int last = i%10;
+			max=max>last?max:last;
+			min=min<last?min:last;
+		}
+		
+		for (int i=min;i<=max ;i++ )
+		{
+			int ct = 0;
+			int n1=n;
+			while (n1>0)
+			{
+				int digit = n1%10;
+				if (i==digit)
+				{
+					ct++;
+				}
+				n1/=10;
+			}
+			if (ct>=1)
+			{
+				System.out.println("The "+i+" is repeted this "+ ct + " times");
+			}
+		}
+	}
+}
