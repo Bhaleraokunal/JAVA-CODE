d027e309534653de5402d0b95eb27ecb97ed871f
Kunal Bhalerao <kunalbhalerao2004@gmail.com>
2025-10-07 23:37:10 +0530

    Rework

--- Arrays/src/arrays/FindingTheMaxSubArray.java
+++ Arrays/src/arrays/FindingTheMaxSubArray.java
@@ -4,7 +4,7 @@ public class FindingTheMaxSubArray {
 
 	
 	public static void main(String[] args) {
-		int a[] = {1,2,3,4,5,1,5,1,-100,-10 } ;
+		int a[] = {-2, 1} ;
 		
 //		int max = maxSubArray(a);
 		
@@ -12,24 +12,20 @@ public class FindingTheMaxSubArray {
 	}
 	
 	public static int maxSubArray(int a[]) {
-		int sum = 0 ;
+		
 		int curSum = 0 ;
 		int max = Integer.MIN_VALUE ;
-		int priviousMax = Integer.MIN_VALUE ; 
 		
 		for (int i = 0; i < a.length; i++) {
-			sum+=a[i];
-			curSum+=a[i] ;
+			curSum+=a[i];
 			
 			if (curSum > max) {
-				priviousMax = max ;
-				max = curSum ;
+				max =  curSum ;
 			}
-			else if (curSum < priviousMax) {
-				curSum = 0 ;
+			if (curSum < 0 ) {
+				curSum = 0;
 			}
 		}
-		
-		return max > sum ? max : sum ;
+		return max ;
 	}
 }
